052e72f2720a3eb1520050ae10aac6227c5adea8
Thibaut <tybo.orcel@gmail.com>
2021-08-24 01:48:55 +0200

    add post modal

---
+++ src/components/Addpost/Addpost.jsx
@@ -0,0 +1,68 @@
+import "./Addpost.scss";
+import { Button, IconButton } from "@material-ui/core";
+import PostAddIcon from "@material-ui/icons/PostAdd";
+import Modal from "@material-ui/core/Modal";
+import { useState } from "react";
+
+const Addpost = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className="Addpost">
+        <IconButton color="primary" onClick={handleOpen}>
+          <PostAddIcon fontSize="large" />
+        </IconButton>
+      </div>
+
+      <Modal open={open} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <div className="post__modal">
+          <form action="/">
+            <div className="item">
+              <p>Topic : </p>
+              <select>
+                <option value="">*Choisissez votre topic*</option>
+                <option value="A">Lifestyle</option>
+                <option value="B">Funny</option>
+                <option value="C">Meme</option>
+                <option value="D">Animals</option>
+                <option value="E">Car</option>
+                <option value="F">Music</option>
+                <option value="G">Movie</option>
+                <option value="H">Sports</option>
+                <option value="I">Video Games</option>
+                <option value="J">Can't Class</option>
+              </select>
+            </div>
+
+            <div className="item">
+              <p>Qu'avez vous en tête ?</p>
+              <textarea rows="5"></textarea>
+            </div>
+
+            <div className="item">
+              <p>L'URL de votre image</p>
+              <input type="text" />
+            </div>
+            <div className="buttons">
+              <Button onClick={handleClose} variant="contained" color="primary">
+                Poster
+              </Button>
+              <Button onClick={handleClose} variant="contained" color="secondary">
+                Fermer
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default Addpost;
--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,17 +1,21 @@
+import Addpost from "../Addpost/Addpost";
 import Post from "../Post/Post";
 import "./Feed.scss";
 
 const Feed = () => {
   return (
-    <div className="Feed">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
+    <>
+      <Addpost />
+      <div className="Feed">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </>
   );
 };
 
