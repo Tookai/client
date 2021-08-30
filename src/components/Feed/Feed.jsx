262f89f724f4b735525afa908f8d9fe8d0ab4182
Thibaut <tybo.orcel@gmail.com>
2021-08-30 14:36:29 +0200

    updated post (dummy data)

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,5 +1,3 @@
-
-
 import Post from "../Post/Post";
 import "./Feed.scss";
 
@@ -7,13 +5,20 @@ const Feed = () => {
   return (
     <>
       <div className="Feed">
-        <Post />
-{/*         <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post /> */}
+        <Post content="Lorem ipsum dolor sit amet consectetur adipisicing elit." image="https://picsum.photos/750/380" />
+        <Post content="Tempore, quisquam dicta." image="https://picsum.photos/500/300" />
+        <Post
+          content="Nisi quae corrupti temporibus error voluptatibus, magnam cumque tempora optio enim saepe quibusdam facere quis itaque, eaque cum dolores?"
+          image="https://picsum.photos/400/300"
+        />
+        <Post
+          content="Repudiandae tempora, ex incidunt ipsum corrupti dolorem harum inventore sequi!"
+          image="https://picsum.photos/500/200"
+        />
+        <Post
+          content=" Nostrum iure debitis provident officia deleniti, fuga magni soluta quas blanditiis accusamus?"
+          image="https://picsum.photos/600/150"
+        />
       </div>
     </>
   );
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,10 @@
 import { Button, IconButton } from "@material-ui/core";
 import ClearIcon from "@material-ui/icons/Clear";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import "./Post.scss";
 import { Link } from "react-router-dom";
 
-const Post = () => {
+const Post = ({ content, image }) => {
   return (
     <div className="Post">
       <div className="top">
@@ -16,20 +17,21 @@ const Post = () => {
           </p>
           <p>À : 00:08 24/08/2021</p>
         </div>
-
-        <IconButton color="secondary" variant="contained" size="small">
-          <ClearIcon />
-        </IconButton>
+        <div>
+          <IconButton color="primary" variant="contained" size="small">
+            <MoreHorizIcon />
+          </IconButton>
+{/*           <IconButton color="secondary" variant="contained" size="small">
+            <ClearIcon />
+          </IconButton> */}
+        </div>
       </div>
 
       <hr />
 
       <div className="content">
-        <p className="text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, accusantium quis aspernatur dolores sint tenetur molestias quo.
-          Quas quo nihil vel dolor repudiandae quasi voluptates, dolorum facere consequatur dignissimos reiciendis?
-        </p>
-        <img src="https://picsum.photos/750/300" alt="random" className="img" loading="lazy" />
+        <p className="text">{content}</p>
+        <img src={`${image}`} alt="random" className="img" loading="lazy" />
       </div>
 
       <hr />
