33c2f247b22ad9e361c6e010a6b9f277b7f84000
Thibaut <tybo.orcel@gmail.com>
2021-09-01 23:27:41 +0200

    redirect if not logged

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import UserPage from "./pages/UserPage/UserPage";
@@ -10,6 +10,7 @@ import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   const queryClient = new QueryClient();
+  const loggedUser = JSON.parse(localStorage.getItem("user"));
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
@@ -22,17 +23,11 @@ function App() {
             <LoginPage />
           </Route>
 
-          <Route path="/user/:id">
-            <UserPage />
-          </Route>
+          <Route path="/user/:id">{!loggedUser ? <Redirect to="/login" /> : <UserPage />}</Route>
 
-          <Route path="/id/:id">
-            <CommentPage />
-          </Route>
+          <Route path="/id/:id">{!loggedUser ? <Redirect to="/login" /> : <CommentPage />}</Route>
 
-          <Route path="/">
-            <HomePage />
-          </Route>
+          <Route path="/">{!loggedUser ? <Redirect to="/login" /> : <HomePage />}</Route>
         </Switch>
       </Router>
       <ReactQueryDevtools />
